dfe21d3931d3fe2544df8b2cafb110de2615b76d
Rafael Coppini <coppini.rafael2@gmail.com>
2026-01-16 17:00:23 -0300

    add zod schema validator and error treatment

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,50 +1,27 @@
-import { RequestHandler } from 'express';
-import * as productsService from '../services/products.service';
+import { RequestHandler } from "express";
+import * as productsService from "../services/products.service";
+import { getProductsSchema } from "../validators/products.validator";
+import { ZodError } from "zod";
 
 export const getProducts: RequestHandler = async (req, res) => {
   try {
-    const limit = Number(req.query.limit) || 10;
-    const page = Number(req.query.page) || 1;
-    const search = req.query.search as string;
-    const lastUpdateDate = req.query.lastUpdateDate as string;
+    const data = getProductsSchema.parse(req.query);
 
-    if (page < 1) {
-      return res.status(400).json({ 
-        error: 'Página deve ser maior que 0',
-        data: null
-      });
-    }
-
-    if (limit < 1 || limit > 100) {
-      return res.status(400).json({ 
-        error: 'Limite deve ser entre 1 e 100',
-        data: null 
-      });
-    }
-
-    if (lastUpdateDate) {
-      const date = new Date(lastUpdateDate);
-      if (isNaN(date.getTime())) {
-        return res.status(400).json({ 
-          error: 'Data inválida. Use formato ISO: YYYY-MM-DD',
-          data: null
-        });
-      }
-    }
-
-
-    const data = await productsService.getProducts({ page, limit, search, lastUpdateDate });
+    const result = await productsService.getProducts(data);
 
     res.status(200).json({
       error: null,
-      data: data.result,
-      count: data.total
+      data: result,
     });
   } catch (err) {
     console.error(err);
 
+    if (err instanceof ZodError) {
+      return res.status(400).json({ error: err, data: null });
+    }
+
     res.status(500).json({
-      error: 'Erro ao buscar produtos',
+      error: "Erro ao buscar produtos",
       data: null,
     });
   }
