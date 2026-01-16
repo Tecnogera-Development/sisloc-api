72be7f9b9ab21b5c136b56dfe67fffd83e2ae98c
Rafael Coppini <coppini.rafael2@gmail.com>
2026-01-16 18:00:44 -0300

    use error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes';
+import { globalErrorHandler } from './middlewares/error.middleware';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
+
+app.use(globalErrorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
